e23a3b34a3aba4ec5e9927e8de620166c53c39af
BHARAT GOSWAMI <bharatgoswami0118@gmail.com>
2020-12-13 13:40:00 +0530

    updated main.js

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ const getInfo = async(event) => {
         try {
 
 
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            let url = `api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=6b7f537a87476c2aa75e933bbc3e0812`
             const response = await fetch(url);
 
             const data = await response.json();
