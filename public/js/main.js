5c7560014d30e7a575a12085b0d9ab40b37db73d
BHARAT GOSWAMI <bharatgoswami0118@gmail.com>
2020-12-13 12:56:28 +0530

    Weather project update in some files

--- public/js/main.js
+++ public/js/main.js
@@ -11,13 +11,13 @@ const getInfo = async(event) => {
 
     let cityVal = cityName.value;
 
-    if(cityVal === ""){
+    if (cityVal === "") {
         city_name.innerText = `Plz write the name before search`;
         datahide.classList.add("data_hide");
-    }else{
+    } else {
 
-        try{
-           
+        try {
+
 
             let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
             const response = await fetch(url);
@@ -32,30 +32,30 @@ const getInfo = async(event) => {
 
             //condition to check sunny or cloudy
             if (tempMood == "Clear") {
-            temp_status.innerHTML =
-                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+                temp_status.innerHTML =
+                    "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
             } else if (tempMood == "Clouds") {
-            temp_status.innerHTML =
-                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+                temp_status.innerHTML =
+                    "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
             } else if (tempMood == "Rain") {
-            temp_status.innerHTML =
-                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+                temp_status.innerHTML =
+                    "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
             } else {
-            temp_status.innerHTML =
-                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+                temp_status.innerHTML =
+                    "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
 
             }
             datahide.classList.remove('data_hide');
             cityVal = "";
-           
-       
-        }catch{
+
+
+        } catch {
             cityVal = " ";
             datahide.classList.add("data_hide");
-            city_name.innerText =  `please enter the proper city name`;
+            city_name.innerText = `please enter the proper city name`;
             console.log('please add the proper city name');
         }
-        
+
     }
 }
 
